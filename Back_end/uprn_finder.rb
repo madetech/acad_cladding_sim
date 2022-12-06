3eb7938069d2cf79fc8b2ea6009063ea67660a17
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-06 15:07:04 +0000

    Add a second passing test

--- Back_end/uprn_finder.rb
+++ Back_end/uprn_finder.rb
@@ -1,3 +1,3 @@
 def properties_in_area(left_bottom_coordinates, top_right_coordinates)
-  ["123456789012"]
+  ["123456789012", "098765432112"]
 end
--- spec/uprn_finder_spec.rb
+++ spec/uprn_finder_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../Back_end/uprn_finder.rb'
 
 describe "UPRN for all properties in area" do
-  it 'returns a UPRN in a given area' do
+  it 'returns the first UPRN of a building in a given area' do
     expect(properties_in_area([0.0085, 51.564], [0.1166, 51.6314])).to include("123456789012")
+  end
+  it "returns the UPRN for a second building in a given area" do
+    expect(properties_in_area([0.0085, 51.564], [0.1166, 51.6314])).to include("098765432112")
   end
 end
