ebb3daa47d4e3753f66c4ff4a36afe8ab72b74b7
Christopher Ashton <christopher.ashton@madetech.com>
2022-12-07 11:21:01 +0000

    remove vendor/bundle 2.7, add gitignore elements

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ gem "rspec", "~> 3.12"
 
 gem "sinatra", "~> 3.0"
 
+
 gem "rack-test", "~> 2.0"
--- vendor/bundle/ruby/2.7.0/gems/rack-protection-3.0.4/lib/rack/protection/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Rack
-  module Protection
-    VERSION = '3.0.3'
-  end
-end
