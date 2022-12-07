994e37a6d7a3c015c4ed2994ea15575e37d3a710
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-07 15:15:35 +0000

    Add file and spec file for retrieval of toid using ngd api

--- spec/building_height_finder_spec.rb
+++ spec/building_height_finder_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../Back_end/building_height_finder.rb'
 
 describe 'Heights for all properties over 18m in the area ' do
-    it 'returns the first height over 18m in an area' do
+    it 'returns the first building with height over 18m in an area' do
         expect(building_height_finder([0.0085, 51.564], [0.1166, 51.6314])).to include(28.37)
+    end
+    it 'returns the second building with height over 18m in an area' do
+        expect(building_height_finder([0.0085, 51.564], [0.1166, 51.6314])).to include(28.79)
+    end
+    it 'does not return buildings with height under 18m in an area' do
+        expect(building_height_finder([0.0085, 51.564], [0.1166, 51.6314])).not_to include(7.49)
     end
 end
