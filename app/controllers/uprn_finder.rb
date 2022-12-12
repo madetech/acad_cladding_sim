2735b4c6aefedf527d19beee661fe9a94168216c
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-12 12:33:05 +0000

    Add linked identifiers api to uprn finder

--- app/controllers/uprn_finder.rb
+++ app/controllers/uprn_finder.rb
@@ -1,3 +1,22 @@
-def uprn_finder()
-  ["100062426862", "100060482125","100060477565"]
+require_relative './toid_from_ngd_api.rb'
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'JSON'
+
+def uprn_finder(left_bottom_coordinates, top_right_coordinates)
+  list_of_toids = tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)
+  list_of_uprns = []
+
+  url = URI("https://api.os.uk/search/links/v1/identifiers/osgb1000012979300?key=uC1mK51yDRfOQsisddsA9y885V0pAfxn")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  
+  request = Net::HTTP::Get.new(url)
+  request["accept"] = 'application/json'
+  
+  response = http.request(request)
+  parsed_json = JSON.parse(response.body)
+  uprn = parsed_json["linkedIdentifiers"][2]
+  puts uprn
 end
--- spec/uprn_finder_spec.rb
+++ spec/uprn_finder_spec.rb
@@ -2,12 +2,12 @@ require_relative '../app/controllers/uprn_finder.rb'
 
 describe "UPRN for all properties in area" do
   it 'returns the correct UPRN of the building with toid osgb1000013035594' do
-    expect(uprn_finder()).to include("100062426862")
+    expect(uprn_finder([0.0085, 51.564], [0.1166, 51.6314])).to include("100062426862")
   end
   it 'returns the correct UPRN of the building with toid osgb1000012978282' do
-    expect(uprn_finder()).to include("100060482125")
+    expect(uprn_finder([0.0085, 51.564], [0.1166, 51.6314])).to include("100060482125")
   end
   it 'returns the correct UPRN of the building with toid osgb1000012979300' do
-    expect(uprn_finder()).to include("100060477565")
+    expect(uprn_finder([0.0085, 51.564], [0.1166, 51.6314])).to include("100060477565")
   end
 end
