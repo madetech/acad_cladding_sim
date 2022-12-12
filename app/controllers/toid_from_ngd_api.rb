2f0a838815106928ba49f5f89028810fbcdc1729
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-12 17:28:37 +0000

    Improve formatting and readability

--- app/controllers/toid_from_ngd_api.rb
+++ app/controllers/toid_from_ngd_api.rb
@@ -3,31 +3,23 @@ require 'net/http'
 require 'openssl'
 require 'JSON'
 
-
+# Return the TOID of buildings over 18m using the OS NGD API
 def tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)
     url = URI("https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-buildingpart/items?bbox=3.545237,50.727083,-3.538381,50.728095&filter=absoluteheightmaximum%20%3E%2018.0&key=CkhkKSjbQD0FoWPnevbA2vdfsBAe1rv8")
-
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
-    
     request = Net::HTTP::Get.new(url)
     request["accept"] = 'application/json'
-    
     response = http.request(request)
+
     parsed_json = JSON.parse(response.body)
+    
     list_of_toids = []
     i = 0
-    while i < 5 do
+    while i < 5 do  # Limit the number to TOIDs returned
         toid = parsed_json["features"][i]["properties"]["toid"]
         list_of_toids.append(toid)
         i += 1
     end
     list_of_toids
-
-    #puts parsed_json
-    #returned_toids = hash["features"][0][5]
-    #puts returned_toids
-
 end
-
-
--- app/controllers/uprn_finder.rb
+++ app/controllers/uprn_finder.rb
@@ -4,20 +4,23 @@ require 'net/http'
 require 'openssl'
 require 'JSON'
 
+# Return the UPRN of buildings using the OS Linked Identifiers API
 def uprn_finder(left_bottom_coordinates, top_right_coordinates)
-  list_of_toids = tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)
-  list_of_uprns = []
+  list_of_toids = tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)  # #  returns an array of TOIDs for buildings over 18m within bounding box coordinates using OS NGD API
+
+  list_of_uprns = []  # Stores returned UPRNs
   
+  # Calls the OS Linked Identifier API on each TOID
   list_of_toids.each do |toid|
     url = URI("https://api.os.uk/search/links/v1/identifiers/#{toid}?key=uC1mK51yDRfOQsisddsA9y885V0pAfxn")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
-  
     request = Net::HTTP::Get.new(url)
     request["accept"] = 'application/json'
-  
     response = http.request(request)
+
     parsed_json = JSON.parse(response.body)
+    
     uprn = parsed_json["linkedIdentifiers"][0]["correlations"][0]["correlatedIdentifiers"][0]["identifier"]
     list_of_uprns.append(uprn)
   end
