a560b37447b827b5323eb0357bc30b3f5fe1430d
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-12 13:00:01 +0000

    Return all uprns to console but fail uprn finder tests

--- app/controllers/uprn_finder.rb
+++ app/controllers/uprn_finder.rb
@@ -8,15 +8,19 @@ def uprn_finder(left_bottom_coordinates, top_right_coordinates)
   list_of_toids = tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)
   list_of_uprns = []
 
-  url = URI("https://api.os.uk/search/links/v1/identifiers/osgb1000012979300?key=uC1mK51yDRfOQsisddsA9y885V0pAfxn")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
+  list_of_toids.each do |toid|
+
+    url = URI("https://api.os.uk/search/links/v1/identifiers/#{toid}?key=uC1mK51yDRfOQsisddsA9y885V0pAfxn")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
   
-  request = Net::HTTP::Get.new(url)
-  request["accept"] = 'application/json'
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
   
-  response = http.request(request)
-  parsed_json = JSON.parse(response.body)
-  list_of_uprns = parsed_json["linkedIdentifiers"][0]["correlations"][0]["correlatedIdentifiers"][0]["identifier"]
- 
+    response = http.request(request)
+    parsed_json = JSON.parse(response.body)
+    uprn = parsed_json["linkedIdentifiers"][0]["correlations"][0]["correlatedIdentifiers"][0]["identifier"]
+    list_of_uprns.append(uprn)
+    puts list_of_uprns
+  end
 end
