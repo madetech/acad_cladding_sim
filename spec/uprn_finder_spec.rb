dc731e1cd0544e6cea7554f368e398ad0d16307c
Sophie Wenban <sophie.wenban@Sophies-MacBook-Pro.local>
2022-12-12 11:48:22 +0000

    Add two passing tests for uprn finder

--- spec/uprn_finder_spec.rb
+++ spec/uprn_finder_spec.rb
@@ -4,4 +4,10 @@ describe "UPRN for all properties in area" do
   it 'returns the correct UPRN of the building with toid osgb1000013035594' do
     expect(uprn_finder()).to include("100062426862")
   end
+  it 'returns the correct UPRN of the building with toid osgb1000012978282' do
+    expect(uprn_finder()).to include("100060482125")
+  end
+  it 'returns the correct UPRN of the building with toid osgb1000012979300' do
+    expect(uprn_finder()).to include("100060477565")
+  end
 end
