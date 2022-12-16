e8c9f40c991ee05812890a162a10ae354506b948
Christopher Ashton <christopher.ashton@madetech.com>
2022-12-16 21:05:37 +0000

    remove some unused lines and comments

--- app/controllers/toid_from_ngd_api.rb
+++ app/controllers/toid_from_ngd_api.rb
@@ -2,14 +2,10 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 require 'json'
-# require '../../app.rb'
 
 # Return the TOID of buildings over 18m using the OS NGD API
 def tall_building_toid_finder(left_bottom_coordinates, top_right_coordinates)
-    # coords = params[:council_value[0]], params[:council_value[1]]
-    # puts coords.to_s.delete! "[]"
     coords =  $coordinates.to_s.delete! "[]"
-    puts coords
     url = URI("https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-buildingpart/items?bbox=#{coords}&filter=absoluteheightmaximum%20%3E%2018.0&key=CkhkKSjbQD0FoWPnevbA2vdfsBAe1rv8")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
